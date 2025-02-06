c21a92ceec6a074cb56f13cd52b45662256ae8eb
Fazliddin <salohiddinovfazliddin265@gmail.com>
2025-02-06 20:00:51 +0500

    dashboard

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () {return view('dashboard.home');});
+Route::get('/register', function () {return view('auth.register');});
+Route::get('/login', function () {return view('auth.login');});
+Route::get('/take-quiz', function () {return view('quiz.take-quiz');});
+Route::get('/about', function () {return view('about');});
