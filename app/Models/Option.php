d1b15c02dd4943224fed17a6d85eef52876f4f66
Fazliddin <salohiddinovfazliddin265@gmail.com>
2025-02-10 19:51:25 +0500

    database

---
+++ app/Models/Option.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Option extends Model
+{
+    protected $fillable = [
+        'name',
+        'value',
+        'is_correct',
+    ];
+
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,24 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\QuizController;
+use App\Http\Controllers\DashboardController;
 
-Route::get('/', function () {return view('dashboard.home');});
-Route::get('/take-quiz', function () {return view('quiz.take-quiz');});
-Route::get('/about', function () {return view('about');});
-Route::get('/create-quiz', function () {return view('dashboard.create-quiz');});
-Route::get('/quizzes', function () {return view('dashboard.quizzes');});
-Route::get('/statistics', function () {return view('dashboard.statistics');});
+Route::get('/', [HomeController::class, 'welcome'])->name('welcome');
+Route::get('/about', [HomeController::class, 'about'])->name('about');
+
+Route::get('/take-quiz', [QuizController::class, 'takeQuiz'])->middleware('auth')->name('take-quiz');
+Route::get('/dashboard/create-quiz', [DashboardController::class, 'create'])->middleware('auth')->name('create-quiz');
+
+
+Route::get('/quiz', function () {return view('dashboard');})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
